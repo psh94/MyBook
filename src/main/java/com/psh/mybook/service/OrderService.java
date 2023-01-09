f32b12249b26f7b30dd40ded93bf2094725680c6
Sanghyeok Park <84213252+psh94@users.noreply.github.com>
2023-01-09 23:33:30 +0900

    Update OrderService.java

--- src/main/java/com/psh/mybook/service/OrderService.java
+++ src/main/java/com/psh/mybook/service/OrderService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public interface OrderService {
 
-    /* 주문 정보 */
+    /* 주문 상품 정보 */
     public List<OrderPageItem> getBooksInfo(List<OrderPageItem> orders);
 
     /* 주문 처리 */
@@ -18,9 +18,6 @@ public interface OrderService {
 
     /* 주문 조회 */
     public Order getOrder(String orderId);
-
-    /* 주문 상품 리스트 */
-    public List<Order> getOrderList(Criteria cri);
 
     /* 주문 총 갯수 */
     public int getOrderTotal(Criteria cri);
